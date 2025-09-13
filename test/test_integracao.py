7cdcebe444275c1d8f18ecf51e38818550db8f62
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-09-12 21:44:03 -0300

    Código concluído para os Testes de Integração

---
+++ test/test_integracao.py
@@ -0,0 +1,26 @@
+import unittest
+from src.calculadora import Calculadora
+
+def test_operacoes_sequenciais(self):
+    calc = Calculadora()
+    calc.somar(2,3)
+    resultado1 = calc.obter_ultimo_resultado()
+
+    calc.multiplicar(resultado1, 4)
+    resultado2 = calc.obter_ultimo_resultado()
+
+    calc.dividir(resultado2, 2)
+    resultado_final = calc.obter_ultimo_resultado()
+
+    self.assertEqual(resultado_final, 10)
+    self.assertEqual(len(calc.historico), 3)
+
+def test_integracao_historico_resultado(self):
+    calc = Calculadora()
+    calc.potencia(2,3)
+    calc.somar(calc.obter_ultimo_resultado(), 2)
+
+    self.assertEqual(calc.obter_ultimo_resultado(), 10)
+    self.assertEqual(len(calc.historico),2)
+    self.assertIn("2 ^ 3 = 8", calc.historico)
+    self.assertIn("8 + 2 = 10", calc.historico)
