d24339d2fce68e56c3ea7f261b3df2c485e1a86d
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-09-15 20:06:59 -0300

    documentação + potencialização

--- tests/test_unidade.py
+++ tests/test_unidade.py
@@ -62,73 +62,94 @@ class TesteUnitarioCalcula(unittest.TestCase):
 
     def test_tipagem_invalida_soma_boolean(self):
         """
-        teste de tipagem invalida string no lugar de numero none
+        teste de tipagem invalida string no lugar de numero boolean
         """
         with self.assertRaises(TypeError):
             self.calc.somar(5, True)  # Boolean no lugar de numero na soma
+
+    def test_tipagem_invalida_potencia_string(self):
+        """
+        teste de tipagem invalida string no lugar de numero potência
+        """
+        with self.assertRaises(TypeError):
+            self.calc.potencia("5", 3)  # String no lugar de numero na soma
+
+    def test_tipagem_invalida_potencia_none(self):
+        """
+        teste de tipagem invalida string no lugar de numero none
+        """
+        with self.assertRaises(TypeError):
+            self.calc.potencia(5, None)  # None no lugar de numero na soma
+
+    def test_tipagem_invalida_potencia_boolean(self):
+        """
+        teste de tipagem invalida string no lugar de numero boolean
+        """
+        with self.assertRaises(TypeError):
+            self.calc.potencia(5, True)  # Boolean no lugar de numero na soma
 
     def test_tipagem_invalida_subtracao_string(self):
         """
         String no lugar de numero na subtração
         """
         with self.assertRaises(TypeError):
-            self.calc.subtrair("5", 3)   
+            self.calc.subtrair("5", 3)
 
     def test_tipagem_invalida_subtracao_none(self):
         """
         None no lugar de numero na subtração
         """
         with self.assertRaises(TypeError):
-            self.calc.subtrair(5, None)  
+            self.calc.subtrair(5, None)
 
     def test_tipagem_invalida_subtracao_boolean(self):
         """
         Boolean no lugar de numero na subtração
         """
         with self.assertRaises(TypeError):
-            self.calc.subtrair(5, True)   
+            self.calc.subtrair(5, True)
 
     def test_tipagem_invalida_multiplicar_string(self):
         """
         String no lugar de numero na multiplicação
         """
         with self.assertRaises(TypeError):
-            self.calc.multiplicar("5", 3)  
+            self.calc.multiplicar("5", 3)
 
     def test_tipagem_invalida_multiplicar_none(self):
         """
         None no lugar de numero na multiplicação
         """
         with self.assertRaises(TypeError):
-            self.calc.multiplicar(5, None) 
+            self.calc.multiplicar(5, None)
 
     def test_tipagem_invalida_multiplicar_boolean(self):
         """
         Boolean no lugar de numero na multiplicação
         """
         with self.assertRaises(TypeError):
-            self.calc.multiplicar(5, True)  
+            self.calc.multiplicar(5, True)
 
     def test_tipagem_invalida_dividir_string(self):
         """
         String no lugar de numero na divisão
         """
         with self.assertRaises(TypeError):
-            self.calc.dividir("5", 3) 
+            self.calc.dividir("5", 3)
 
     def test_tipagem_invalida_dividir_none(self):
         """
         None no lugar de numero na divisão
         """
         with self.assertRaises(TypeError):
-            self.calc.dividir(10, None)  
+            self.calc.dividir(10, None)
 
     def test_tipagem_invalida_dividir_boolean(self):
         """
         Boolean no lugar de numero na divisão
         """
         with self.assertRaises(TypeError):
-            self.calc.dividir(5, True)  # 
+            self.calc.dividir(5, True)  #
 
     def test_consistencia_historico_1(self):
         """
@@ -147,7 +168,7 @@ class TesteUnitarioCalcula(unittest.TestCase):
         self.assertEqual(len(self.calc.historico), 2)
         self.assertIn("2 + 3 = 5", self.calc.historico)
         self.assertIn("4 * 5 = 20", self.calc.historico)
-    
+
     def test_inicialização(self):
         """
         Garante que a inicialização foi feita corretamente
@@ -162,7 +183,7 @@ class TesteUnitarioCalcula(unittest.TestCase):
         realizada a soma
         """
         calc = Calculadora()
-        calc.somar(1 , 1)
+        calc.somar(1, 1)
         self.assertEqual(len(calc.historico), 1)
         calc.limpar_historico()
         self.assertEqual(len(calc.historico), 0)
@@ -173,7 +194,7 @@ class TesteUnitarioCalcula(unittest.TestCase):
         realizada a subtração
         """
         calc = Calculadora()
-        calc.subtrair(1 , 1)
+        calc.subtrair(1, 1)
         self.assertEqual(len(calc.historico), 1)
         calc.limpar_historico()
         self.assertEqual(len(calc.historico), 0)
@@ -184,44 +205,44 @@ class TesteUnitarioCalcula(unittest.TestCase):
         realizada a divisão
         """
         calc = Calculadora()
-        calc.dividir(1 , 1)
+        calc.dividir(1, 1)
         self.assertEqual(len(calc.historico), 1)
         calc.limpar_historico()
         self.assertEqual(len(calc.historico), 0)
-    
+
     def test_modificacao_historico_multiplicacao(self):
         """
         Valida a inserção de informações e exclusão de informação no histórico quando
         realizada a multiplicação
         """
         calc = Calculadora()
-        calc.multiplicar(1 , 1)
+        calc.multiplicar(1, 1)
         self.assertEqual(len(calc.historico), 1)
         calc.limpar_historico()
         self.assertEqual(len(calc.historico), 0)
-    
+
     def test_modificacao_historico_potencializacao(self):
         """
         Valida a inserção de informações e exclusão de informação no histórico quando
         realizada a potencialização
         """
         calc = Calculadora()
-        calc.potencia(1 , 1)
+        calc.potencia(1, 1)
         self.assertEqual(len(calc.historico), 1)
         calc.limpar_historico()
         self.assertEqual(len(calc.historico), 0)
-    
+
     def test_limite_inferior(self):
         """
         Teste de comportamento com valores minimos
         """
         calc = Calculadora()
         # Teste com zero
-        resultado = calc.somar(0,5)
+        resultado = calc.somar(0, 5)
         self.assertEqual(resultado, 5)
         # Teste com números negativos muito pequenos
-        resultado = calc.multiplicar(-1e-10 , 2)
-        self.assertEqual(resultado , -2e-10)
+        resultado = calc.multiplicar(-1e-10, 2)
+        self.assertEqual(resultado, -2e-10)
 
     def test_divisao_por_zero(self):
         """
@@ -229,7 +250,7 @@ class TesteUnitarioCalcula(unittest.TestCase):
         """
         calc = Calculadora()
         with self.assertRaises(ValueError):
-            calc.dividir(10,0)
+            calc.dividir(10, 0)
 
     def test_mensagem_erro_soma(self):
         """
@@ -240,7 +261,7 @@ class TesteUnitarioCalcula(unittest.TestCase):
             calc.somar(5, "1")
         except ValueError as e:
             self.assertEqual(str(e), "Argumentos devem ser numeros")
-        
+
     def test_mensagem_erro_subtracao(self):
         """
         Valida mensagem de erro da subtração
@@ -270,7 +291,7 @@ class TesteUnitarioCalcula(unittest.TestCase):
             calc.dividir(5, "1")
         except ValueError as e:
             self.assertEqual(str(e), "Argumentos devem ser numeros")
-        
+
     def test_mensagem_erro_potencia(self):
         """
         Valida mensagem de erro da potência
@@ -280,10 +301,10 @@ class TesteUnitarioCalcula(unittest.TestCase):
             calc.potencia(5, "1")
         except ValueError as e:
             self.assertEqual(str(e), "Argumentos devem ser numeros")
-        
+
     def test_mensagem_erro_div0(self):
         calc = Calculadora()
         try:
-            calc.dividir(5 ,0)
+            calc.dividir(5, 0)
         except ValueError as e:
             self.assertEqual(str(e), "Divisao por zero nao permitida")
